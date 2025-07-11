87c053938b9e220f8ce7ef7c72727c4cb5a4cc40
Xuwang Yin <xuwangyin@gmail.com>
2025-07-10 22:19:59 -0700

    Updated imagenet path

--- utils/datasets/paths.py
+++ utils/datasets/paths.py
@@ -62,7 +62,7 @@ def get_tiny_images_lmdb():
     raise NotImplementedError()
 
 def get_imagenet_path():
-    path = "/home/xuwang/ImageNet"
+    path = "./ImageNet"
     return path
 
 def get_imagenet_o_path():
