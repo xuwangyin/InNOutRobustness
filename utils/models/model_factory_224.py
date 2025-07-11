b05a5b80917dbfe69b404312921c6af09c8dfacd
Xuwang Yin <xuwangyin@gmail.com>
2025-07-10 22:14:32 -0700

    Updated timm interface

--- utils/models/model_factory_224.py
+++ utils/models/model_factory_224.py
@@ -1,48 +1,48 @@
 from timm.models import create_model
 from resnet import resnet50, wide_resnet50_2
+from utils.models.model_factory_32 import parse_params
 
-def build_model(model_name, num_classes, **kwargs):
+def build_model(model_name, num_classes, model_params=None, **kwargs):
     model_name = model_name.lower()
+    model_config = parse_params(model_params)
+    print(f'Model params: {model_config}')
+    
     if model_name == 'sslresnext50':
         model_name = 'SSLResNext50'
-        model = create_model('ssl_resnext50_32x4d', num_classes=num_classes, **kwargs)
-        config = dict(name=model_name, **kwargs)
-    elif model_name == 'resnet50':
+        model = create_model('ssl_resnext50_32x4d', num_classes=num_classes, **model_config)
+    elif model_name.startswith('resnet50'):
         model_name = 'ResNet50'
-        print(kwargs)
-        model = resnet50(num_classes=num_classes)
-        # model = create_model('resnet50', num_classes=num_classes, **kwargs)
-        config = dict(name=model_name, **kwargs)
-    elif model_name == 'resnet50_timm':
-        model_name = 'ResNet50_timm'
-        print(kwargs)
-        print('using timm')
-        model = create_model('resnet50', num_classes=num_classes, **kwargs)
-        config = dict(name=model_name, **kwargs)
+        model = create_model('resnet50', num_classes=num_classes, **model_config)
     elif model_name == 'wide_resnet50_2':
         model_name = 'WideResNet_50_2'
-        print(kwargs)
         model = wide_resnet50_2(num_classes=num_classes)
-        config = dict(name=model_name, **kwargs)
     elif model_name == 'wide_resnet50_2_timm':
-        print('using timm')
         model_name = 'WideResNet_50_2_timm'
-        model = create_model('wide_resnet50_2', num_classes=num_classes, **kwargs)
-        config = dict(name=model_name, **kwargs)
+        model = create_model('wide_resnet50_2', num_classes=num_classes, **model_config)
     elif model_name == 'tresnetm':
         model_name = 'TResNet-M'
-        model = create_model('tresnet_m', num_classes=num_classes, **kwargs)
-        config = dict(name=model_name, **kwargs)
+        model = create_model('tresnet_m', num_classes=num_classes, **model_config)
     elif model_name == 'seresnext26t':
         model_name = 'SE-ResNeXt-26-T'
-        model = create_model('seresnext26t_32x4d', num_classes=num_classes, **kwargs)
-        config = dict(name=model_name, **kwargs)
+        model = create_model('seresnext26t_32x4d', num_classes=num_classes, **model_config)
     elif model_name == 'seresnext50':
         model_name = 'SE-ResNeXt-50'
-        model = create_model('seresnext50_32x4d', num_classes=num_classes, **kwargs)
-        config = dict(name=model_name, **kwargs)
+        model = create_model('seresnext50_32x4d', num_classes=num_classes, **model_config)
+    elif model_name == 'convnext_tiny':
+        model_name = 'ConvNeXt-Tiny'
+        model = create_model('convnext_tiny', num_classes=num_classes, **model_config)
+    elif model_name == 'convnext_small':
+        model_name = 'ConvNeXt-Small'
+        model = create_model('convnext_small', num_classes=num_classes, **model_config)
+    elif model_name == 'convnext_base':
+        model_name = 'ConvNeXt-Base'
+        model = create_model('convnext_base', num_classes=num_classes, **model_config)
+    elif model_name == 'convnext_large':
+        model_name = 'ConvNeXt-Large'
+        model = create_model('convnext_large', num_classes=num_classes, **model_config)
     else:
         print(f'Net {model_name} not supported')
         raise NotImplemented()
 
+    config = dict(name=model_name, **model_config)
     return model, model_name, config, 224
